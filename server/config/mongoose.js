6e98c5cba5094815f81a7dbf37aebf5719d4c5bd
deepjyoti941 <deepjyoti941@gmail.com>
2014-03-02 23:09:41 +0530

    preparing for login

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,4 +7,20 @@ module.exports = function(config) {
   db.once('open', function callback() {
     	console.log('mytutorial db opened');
  	});
+
+  //creating schema for users
+  var userSchema = mongoose.Schema({
+  	firstName: String,
+  	lastName: String,
+  	userName: String
+  });
+
+  var User = mongoose.model('User', userSchema);
+
+  User.find({}).exec(function(err, collection){
+  	if(collection.length === 0) {
+  		User.create({firstName:'deepjyoti',lastName:'khakhlary', userName:'deepjyoti941'});
+  		User.create({firstName:'deep', lastName:'khakhlary', userName:'deep88'});
+  	}
+  })
  }

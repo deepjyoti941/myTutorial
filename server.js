2e98f29c140172bd9171b77f1105aa865762dbbb
deepjyoti941 <deepjyoti941@gmail.com>
2014-03-03 23:57:02 +0530

    creating server side login authentication using passport module

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express'),
-	mongoose = require('mongoose');
+	mongoose = require('mongoose'),
+	passport = require('passport');
+	LocalStrategy = require('passport-local').Strategy;
 
 //creating node enviroment variable
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -12,7 +14,37 @@ require('./server/config/express')(app,config);
 
 require('./server/config/mongoose')(config);
 
+var User = mongoose.model('User');
+passport.use(new LocalStrategy(
+	function (username, password, done) {
+		User.findOne({username: username}).exec(function(err, user){
+			if (user) {
+				return done(null, user);
+			}else {
+				return done(null, false);
+			}
+		});
+	}
+));
+
+passport.serializeUser(function(user, done) {
+	if(user) {
+		done(null, user._id);
+	}
+});
+
+passport.deserializeUser(function(id, done) {
+	User.findOne({_id:id}).exec(function(err, user){
+		if(user) {
+			return done(null, user);
+		} else {
+			return done(null, false);
+		}
+	})
+});
+
 require('./server/config/routes')(app);
 
 app.listen(config.port);
+
 console.log('Listening on port ' +config.port+ '....');
--- server/config/express.js
+++ server/config/express.js
@@ -1,6 +1,7 @@
 var express = require('express'),
-	stylus = require('stylus'); //for static route handling i.e serving static files,css files	
-
+	stylus = require('stylus'), //for static route handling i.e serving static files,css files	
+	passport = require('passport');
+	
 module.exports = function(app, config) {
 	function compile(str, path) {
 		return stylus(str).set('filename', path);
@@ -10,7 +11,11 @@ module.exports = function(app, config) {
 		app.set('views' , config.rootPath + '/server/views');
 		app.set('view engine', 'jade');
 		app.use(express.logger('dev'));
+		app.use(express.cookieParser());
 		app.use(express.bodyParser());
+		app.use(express.session({secret:'global vision unicorns'}));
+		app.use(passport.initialize());
+		app.use(passport.session());
 		app.use(stylus.middleware(
 			{
 				src: config.rootPath + '/public',

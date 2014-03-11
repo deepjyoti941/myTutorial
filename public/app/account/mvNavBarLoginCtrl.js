1a632839a967c7fa78419222d28a33fc9ff876e6
deepjyoti941 <deepjyoti941@gmail.com>
2014-03-11 21:29:39 +0530

    preparing server side login with client side notification using toastr

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,10 +1,11 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http) {
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier) {
   $scope.signin = function(username, password) {
     $http.post('/login', {username:username, password:password}).then(function(response){
       if(response.data.success) {
-        console.log('logged in');
+      	mvIdentity.currentUser = response.data.user;
+      	mvNotifier.notify('You have successfully logged in!');
       } else {
-        console.log('failure');
+        mvNotifier.notify('incorrect username/password!');
       }
     })
   }

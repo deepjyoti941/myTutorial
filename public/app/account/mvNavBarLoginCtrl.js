23cf41e5e3461f1ab7a42e85c3c6a07aebb4ae82
deepjyoti941 <deepjyoti941@gmail.com>
2014-03-11 22:58:12 +0530

    improving client side login with differnt Auth service

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,12 +1,13 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier) {
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth) {
+
+  $scope.identity = mvIdentity;
   $scope.signin = function(username, password) {
-    $http.post('/login', {username:username, password:password}).then(function(response){
-      if(response.data.success) {
-      	mvIdentity.currentUser = response.data.user;
-      	mvNotifier.notify('You have successfully logged in!');
-      } else {
-        mvNotifier.notify('incorrect username/password!');
-      }
-    })
+  	mvAuth.authenticateUser(username, password).then(function(success){
+  		if(success) {
+  			mvNotifier.notify('You have successfully logged in!');
+  		} else {
+  		    mvNotifier.notify('Username/Password combination incorrect');	
+  		}
+  	});
   }
 });

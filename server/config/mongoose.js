e0710ff5a158e59de9aa1ddb45237d786ebd7e73
deepjyoti941 <deepjyoti941@gmail.com>
2014-03-11 23:38:49 +0530

    implementing secure passwd using hashing and salt

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,4 +1,5 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
+  crypto = require('crypto');
 
 module.exports = function(config) {
   mongoose.connect(config.db);
@@ -12,15 +13,41 @@ module.exports = function(config) {
   var userSchema = mongoose.Schema({
   	firstname: String,
   	lastname: String,
-  	username: String
+  	username: String,
+    salt: String,
+    hashed_pwd: String
   });
 
+  userSchema.methods = {
+    authenticate: function(passwordToMatch) {
+      return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+    }
+  }
   var User = mongoose.model('User', userSchema);
 
   User.find({}).exec(function(err, collection){
   	if(collection.length === 0) {
-  		User.create({firstname:'deepjyoti',lastname:'khakhlary', username:'deepjyoti941',});
-  		User.create({firstname:'deep', lastname:'khakhlary', username:'deep88'});
+      var salt, hash;
+      salt = createSalt();
+      hash = hashPwd(salt, '123');
+  		User.create({firstname:'deepjyoti',lastname:'khakhlary', username:'deepjyoti941',salt:salt, hashed_pwd:hash });
+  		salt = createSalt();
+      hash = hashPwd(salt, '456');
+      User.create({firstname:'deep', lastname:'khakhlary', username:'deep88',salt:salt, hashed_pwd:hash});
   	}
   });  
  }
+
+function createSalt() {
+  return crypto.randomBytes(128).toString('base64');
+}
+
+function hashPwd(salt, pwd) {
+  var hmac = crypto.createHmac('sha1', salt);
+  return hmac.update(pwd).digest('hex');
+}
+
+
+
+
+

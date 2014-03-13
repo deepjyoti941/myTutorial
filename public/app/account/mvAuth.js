9ea00b8b64b66a018cfd2d08983e874d56435a71
deepjyoti941 <deepjyoti941@gmail.com>
2014-03-13 19:52:31 +0530

    creating server-side code for user profile page

--- public/app/account/mvAuth.js
+++ public/app/account/mvAuth.js
@@ -1,74 +1,70 @@
 angular.module('app').factory('mvAuth', function($http, mvIdentity, $q, mvUser) {
+  return {
+    authenticateUser: function(username, password) {
+      var dfd = $q.defer();
+      $http.post('/login', {username:username, password:password}).then(function(response) {
+        if(response.data.success) {
+          var user = new mvUser();
+          angular.extend(user, response.data.user);
+          mvIdentity.currentUser = user;
+          dfd.resolve(true);
+        } else {
+          dfd.resolve(false);
+        }
+      });
+      return dfd.promise;
+    },
 
-	return {
-		authenticateUser: function(username, password) {
-			var dfd	= $q.defer();
-			$http.post('/login', {username:username, password:password}).then(function(response){
-		      if(response.data.success) {
-		      	var user = new mvUser();
-		      	angular.extend(user, response.data.user)
-		      	mvIdentity.currentUser = user;
-		      	dfd.resolve(true);
-		      } else {
-		      	dfd.resolve(false);
-		      }
-		    });
-		    return dfd.promise;
-		},
+    createUser: function(newUserData) {
+      var newUser = new mvUser(newUserData);
+      var dfd = $q.defer();
 
-		createUser:function(newUserData) {
-			var newUser = new mvUser(newUserData);
-			var dfd = $q.defer();
-			newUser.$save().then(function() {
-				mvIdentity.currentUser = newUser;
-				dfd.resolve();
-			}, function(response) {
-				dfd.reject(response.data.reason);
-			});
-			return dfd.promise;
-		},
+      newUser.$save().then(function() {
+        mvIdentity.currentUser = newUser;
+        dfd.resolve();
+      }, function(response) {
+        dfd.reject(response.data.reason);
+      });
 
-		updateCurrentUser: function(newUserData) {
-			var dfd = $q.defer();
+      return dfd.promise;
+    },
 
-			var clone = angular.copy(mvIdentity.currentUser);
-			angular.extend(clone, newUserData);
-			clone.$update().then(function() {
-				mvIdentity.currentUser = clone;
-				dfd.resolve();
-			}, function(response){
-				dfd.reject(response.data.reason);
-			});
-			return dfd.promise;
-		},
+    updateCurrentUser: function(newUserData) {
+      var dfd = $q.defer();
 
-		logoutUser: function() {
-			var dfd = $q.defer();
-			$http.post('/logout', {logout:true}).then(function() {
-				mvIdentity.currentUser = undefined;
-				dfd.resolve();
-			});
-			return dfd.promise;
-		},
-		
-		authorizecurrentUserForRoute: function(role) {
-			if(mvIdentity.isAuthorized(role)) {
-				return true;
-			} else {
-				return $q.reject('not authorized');
-			}
-		},
+      var clone = angular.copy(mvIdentity.currentUser);
+      angular.extend(clone, newUserData);
+      clone.$update().then(function() {
+        mvIdentity.currentUser = clone;
+        dfd.resolve();
+      }, function(response) {
+        dfd.reject(response.data.reason);
+      });
+      return dfd.promise;
+    },
 
-		authorizeAuthenticatedUserForRoute: function() {
-			if (mvIdentity.isAuthenticated()) {
-				return true;
-			} else {
-				return $q.reject('not authorized');
-			}
-		}
+    logoutUser: function() {
+      var dfd = $q.defer();
+      $http.post('/logout', {logout:true}).then(function() {
+        mvIdentity.currentUser = undefined;
+        dfd.resolve();
+      });
+      return dfd.promise;
+    },
+    authorizeCurrentUserForRoute: function(role) {
+      if(mvIdentity.isAuthorized(role)) {
+        return true;
+      } else {
+        return $q.reject('not authorized');
+      }
 
-	}
+    },
+    authorizeAuthenticatedUserForRoute: function() {
+      if(mvIdentity.isAuthenticated()) {
+        return true;
+      } else {
+        return $q.reject('not authorized');
+      }
+    }
+  }
 });
-
-
-
--- public/app/account/mvUser.js
+++ public/app/account/mvUser.js
@@ -1,12 +1,11 @@
 angular.module('app').factory('mvUser', function($resource) {
-	
-	var UserResource = $resource('/api/users/:id', {_id: "@id"},{
-		update: {method:'PUT', isArray:false}
-	});
+  var UserResource = $resource('/api/users/:id', {_id: "@id"}, {
+    update: {method:'PUT',isArray:false}
+  });
 
-	//adding isAdmin method to every instance of UserResource
-	UserResource.prototype.isAdmin = function() {
-		return this.roles && this.roles.indexOf('admin') > -1;
-	}
-	return UserResource;
+  UserResource.prototype.isAdmin = function() {
+    return this.roles && this.roles.indexOf('admin') > -1;
+  }
+
+  return UserResource;
 });
